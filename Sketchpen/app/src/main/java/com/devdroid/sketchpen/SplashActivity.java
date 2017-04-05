db4c783f5ae503b7ea30e0eaab42b93adb5d8a91
shankarprasad <eshankarprasad@gmail.com>
2017-04-05 20:10:17 +0530

    Changes for v4.3.0

--- Sketchpen/app/src/main/java/com/devdroid/sketchpen/SplashActivity.java
+++ Sketchpen/app/src/main/java/com/devdroid/sketchpen/SplashActivity.java
@@ -1,18 +1,48 @@
 package com.devdroid.sketchpen;
 
+import android.*;
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 
 public class SplashActivity extends Activity {
+
+    private static final int PERMISSIONS_REQUEST_WRITE_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && this.checkSelfPermission(android.Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_CONTACTS}, PERMISSIONS_REQUEST_WRITE_STORAGE);
+        } else {
+            launchLandingPage(1500);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_WRITE_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                launchLandingPage(0);
+            } else {
+                Toast.makeText(this, "Until you grant the permission, you can'nt launch the app", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void launchLandingPage(int delay) {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,6 +52,6 @@ public class SplashActivity extends Activity {
                     startActivity(SketchPenActivity.getIntent(SplashActivity.this));
                 }
             }
-        }, 1500);
+        }, delay);
     }
 }
